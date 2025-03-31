69cfb8248d9f475b25edacbe15b171baa54bf3b3
AlexeyKuzh <oleksijku20@gmail.com>
2025-03-31 20:17:51 +0300

    Initial commit

---
+++ ToDoA/src/main/java/com/example/ToDoA/ToDoAApplication.java
@@ -0,0 +1,13 @@
+package com.example.ToDoA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAApplication.class, args);
+	}
+
+}
---
+++ ToDoA/src/test/java/com/example/ToDoA/ToDoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ToDoA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
