a524782d3ac8c67cb677502b8c4eab35e26cd2a4
AlexeyKuzh <oleksijku20@gmail.com>
2025-04-12 12:06:55 +0300

    first commit part 3.2  small changes, created GetAllGroups in Controller Service

--- ToDoA/src/main/java/com/example/ToDoA/service/GroupService.java
+++ ToDoA/src/main/java/com/example/ToDoA/service/GroupService.java
@@ -1,13 +1,28 @@
 package com.example.ToDoA.service;
 
+import com.example.ToDoA.models.Group;
 import com.example.ToDoA.repository.GroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class GroupService {
 
-    GroupRepository groupRepository;
+    private final GroupRepository groupRepository;
+    @Autowired
+    public GroupService(GroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
 
+    public List<Group> findAllGroups(){
+        return groupRepository.findAll();
+    }
 
-
+    public Group findByIdGroup(int id){
+        Group by = groupRepository.findBy(id);
+        return by;
+    }
 }
