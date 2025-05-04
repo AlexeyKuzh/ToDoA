6a03129958fcacc6c206d88f9c543d131ac52e8b
AlexeyKuzh <oleksijku20@gmail.com>
2025-05-04 12:34:47 +0300

    commit 4 part bd migration items

--- ToDoA/src/main/java/com/example/ToDoA/controller/ItemController.java
+++ ToDoA/src/main/java/com/example/ToDoA/controller/ItemController.java
@@ -1,4 +1,7 @@
 package com.example.ToDoA.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ItemController {
 }
--- ToDoA/src/main/java/com/example/ToDoA/service/ItemService.java
+++ ToDoA/src/main/java/com/example/ToDoA/service/ItemService.java
@@ -1,4 +1,7 @@
 package com.example.ToDoA.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ItemService {
 }
